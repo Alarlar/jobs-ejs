4aa8217bc5eb83199814375fa3e996e39b3abd91
Alarlar <52812499+Alarlar@users.noreply.github.com>
2025-10-22 21:48:53 -0700

    added session, secret word is stored and retrieved in the session, adding session  to mongo

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
 const express = require("express");
 require("express-async-errors");
+require("dotenv").config(); // to load the .env file into the process.env object
+const session = require("express-session"); // Тут сидит объект сессия
+const MongoDBStore = require("connect-mongodb-session")(session);
+const url = process.env.MONGO_URI;
 
+// создание приложения Express
 const app = express();
+
+const store = new MongoDBStore({
+  // may throw an error, which won't be caught
+  uri: url,
+  collection: "mySessions",
+});
+store.on("error", function (error) {
+  console.log(error);
+});
+
+const sessionParms = {
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  store: store,
+  cookie: { secure: false, sameSite: "strict" }, // верно для локальной разработки (HTTP).
+};
+
+if (app.get("env") === "production") {
+  app.set("trust proxy", 1); // trust first proxy
+  sessionParms.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sessionParms));
+
+// // Настройка сессий, Фактически это опции для управления тем, когда сессия записывается на сервер.
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false, // Функция сохранения изменённой сессии - сохранять только если данные сессии изменились
+//     saveUninitialized: true, // Функция создания новой сессии - сохранять новую пустую сессию сразу
+//   })
+// );
 
 app.set("view engine", "ejs");
 // app.use(require("body-parser").urlencoded({ extended: true }));
@@ -11,12 +49,15 @@ app.use(express.urlencoded({ extended: true })); // для HTML-форм
 app.use(express.json()); // для JSON
 
 // secret word handling
-let secretWord = "syzygy";
+// let secretWord = "syzygy"; - storing secretWord as a global variable
 app.get("/secretWord", (req, res) => {
-  res.render("secretWord", { secretWord });
+  if (!req.session.secretWord) {
+    req.session.secretWord = "syzygy";
+  }
+  res.render("secretWord", { secretWord: req.session.secretWord });
 });
 app.post("/secretWord", (req, res) => {
-  secretWord = req.body.secretWord;
+  req.session.secretWord = req.body.secretWord;
   res.redirect("/secretWord");
 });
 
